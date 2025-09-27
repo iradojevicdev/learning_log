03a5520f6da3d5d8d60dbc11cf4779d46fce0382
Ivan Radojevic <radojevicivan004@gmail.com>
2025-09-27 14:36:51 +0200

    Prepare for Render deploy

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,6 +128,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #my settings
 LOGIN_URL = 'users:login'
-#heroku settings
-import django_heroku
-django_heroku.settings(locals())
